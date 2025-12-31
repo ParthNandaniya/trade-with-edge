5bcd4649c67011e62c9fc4af4682b6da48d2b52c
ParthNandaniya <parthnandaniya@gmail.com>
2025-12-30 19:40:58 -0800

    files for open ai model

---
+++ dashboard/src/utils/files.ts
@@ -0,0 +1,7 @@
+import stevenDuxALlInterviewsTranscript from '../files/steven_dux_all_interviews_transcript.txt'
+import stevenDuxALlYoutubeVideosTranscript from '../files/steven_dux_all_youtube_videos_transcript.txt'
+
+export {
+    stevenDuxALlInterviewsTranscript,
+    stevenDuxALlYoutubeVideosTranscript,
+}
