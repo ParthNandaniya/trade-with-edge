5f2509928ca7a0da4a49d9bfcef34ca609c40901
ParthNandaniya <parthnandaniya@gmail.com>
2025-12-30 19:44:43 -0800

    instructions added for model

--- dashboard/src/utils/files.ts
+++ dashboard/src/utils/files.ts
@@ -1,7 +1,9 @@
+import instructions from '../files/instructions.txt'
 import stevenDuxALlInterviewsTranscript from '../files/steven_dux_all_interviews_transcript.txt'
 import stevenDuxALlYoutubeVideosTranscript from '../files/steven_dux_all_youtube_videos_transcript.txt'
 
 export {
+    instructions,
     stevenDuxALlInterviewsTranscript,
     stevenDuxALlYoutubeVideosTranscript,
 }
